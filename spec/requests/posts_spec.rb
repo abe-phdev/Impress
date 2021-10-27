f289c5b1ddc6e9a2444087f4c80c5c6d6740fe23
Raymond <raymond@philippinedev.com>
2021-10-27 18:52:09 +0800

    added test for the creation of new post

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "/posts/new" do
+    it 'succeeds' do
+      get new_post_path
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "/posts/create" do
+    let(:params) do {
+      post: {
+        title: "Sample Title",
+        body: "Sample post body."
+      }
+    }
+    end
+
+    context "with valid params" do
+      subject { post posts_path, params: params }
+
+      it 'create a post' do
+        expect{ subject }.to change { Post.count }.by(1)
+      end
+    end
+
+    context "with invalid params" do
+      let(:title) { nil }
+      let(:body) { nil }
+
+      it 'fails to create a post' do
+        post posts_path, params: params
+        expect(response.status).to eq 302
+      end
+    end
   end
 end
